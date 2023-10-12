9245ff2cb0ffe235b7dd4df7c69a0322e8338e62
seohyundeveloper <seoandjisister77@gmail.com>
2023-10-13 01:02:31 +0900

    slide 주석 삭제

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,6 +1,3 @@
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
-// import Slider from 'react-slick';
 import React from 'react';
 import {Swiper, SwiperSlide } from 'swiper/react';
 import {Autoplay} from 'swiper/modules';
@@ -68,51 +65,6 @@ const Carousel = (() =>{
       </Swiper>
     </div>
   )
-  // const settings = {
-  //   dots: false,
-  //   infinite: true,
-  //   autoPlay: true,
-  //   speed: 300,
-  //   slidesToShow: 5,
-  //   slidesToScroll: 1,
-  //   cssEase: "linear"
-  // }
-  // return (
-  //   <>
-  //     <Slider {...settings}>
-  //           <div className="slick__text">
-  //             <h3>outtake</h3>
-  //           </div>
-  //           <div className="slick__text">
-  //             <h3>SOODA</h3>
-  //           </div>
-  //           <div className="slick__text">
-  //             <h3>Spain Vibe Tour</h3>
-  //           </div>
-  //           <div className="slick__text">
-  //             <h3>West Pacific</h3>
-  //           </div>
-  //           <div className="slick__text">
-  //             <h3>Map</h3>
-  //           </div>
-  //           <div className="slick__text">
-  //             <h3>MAPO</h3>
-  //           </div>
-  //           <div className="slick__text">
-  //             <h3>Cornerstone College</h3>
-  //           </div>
-  //           <div className="slick__text">
-  //             <h3>GIGU</h3>
-  //           </div>
-  //           <div className="slick__text"> 
-  //             <h3>LET EAT GO</h3>
-  //           </div>
-  //           <div className="slick__text">
-  //             <h3>Byeol Bam</h3>
-  //           </div>
-  //     </Slider>
-  //   </>
-  // )
 })
 
 export default Carousel;
